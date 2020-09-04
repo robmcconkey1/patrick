451b7b2dd048c3d83ee506e439d44942cdcb01b9
Rob McConkey <robmcconkey@Robs-MacBook-Pro-3.local>
2020-09-04 19:51:07 +0100

    new upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1> McConkey Architects</h1>
+
+
+      <p> Testing TestingTestingTestingTestingTestingTestingTesting
+       </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
