abe233b4603f10300d85b9b7f53322dfea744e8e
Rob McConkey <robmcconkey@Robs-MacBook-Pro-3.local>
2020-09-04 21:00:40 +0100

    Updates

---
+++ precache-manifest.87b4f51ef87e4f72911a6e2224cac84a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2586f7eb2275213a7ce80b18adef1944",
+    "url": "/patrick/index.html"
+  },
+  {
+    "revision": "d0cd3affe7baa5e60cae",
+    "url": "/patrick/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30e171ed6466db8fb853",
+    "url": "/patrick/static/js/2.28fba093.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/patrick/static/js/2.28fba093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0cd3affe7baa5e60cae",
+    "url": "/patrick/static/js/main.92b340a1.chunk.js"
+  },
+  {
+    "revision": "63264c0fba4b558edc3e",
+    "url": "/patrick/static/js/runtime-main.27793dc3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/patrick/static/media/logo.5d5d9eef.svg"
+  }
+]);
