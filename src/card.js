be5fafe3eb083e61b3857e203e5d0de6296afc52
Rob McConkey <robmcconkey@Robs-MacBook-Pro-3.local>
2020-09-05 20:53:02 +0100

    change

---
+++ src/card.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link'
+import paris from '.travel4.jpg' 
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 240,
+  },
+});
+
+ function CardParis() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={paris}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+
+            <Link href="/" >
+               Paris
+             </Link>
+
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            The best way to plan a trip to Paris
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+
+
+}
+export default CardParis
