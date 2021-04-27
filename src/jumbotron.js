c38f0afae0563dd2a57a63f233756c486fa1a0cc
Rob McConkey <robmcconkey@Robs-MacBook-Pro-3.local>
2021-04-27 21:38:06 +0100

    header h3 change

--- src/jumbotron.js
+++ src/jumbotron.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Jumbotron as Jumbo, Container} from 'react-bootstrap';
 import styled from 'styled-components';
 import london1 from './travel4.jpg';
+import './App.css';
 
 
 const Styles = styled.div`
